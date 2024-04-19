42d0c05bfcaf79be94781bc547f67ef76992f1bd
tamilz <tamilcrea@gmail.com>
2024-04-19 21:43:44 +0530

    added changes

--- app/category/[categoryId]/page.js
+++ app/category/[categoryId]/page.js
@@ -28,8 +28,8 @@ const Posts = () => {
   return (
     <div className="max-w-2x1 mx-auto bg-white shadow-md rounded-lg overflow-hidden p-6" style={{marginLeft: '200px', marginTop: '50px', padding: '10px', width: '60rem'}}>
     <p className="text-gray-600 mb-4">Posts</p>
-    { posts && posts.length > 0 ?  (posts.map((post) => (
-      <div style={{marginLeft: '20px', marginTop: '25px'}}>
+    { posts && posts.length > 0 ?  (posts.map((post, index) => (
+      <div key={index} style={{marginLeft: '20px', marginTop: '25px'}}>
       <h2 className="text-x1 font-semibold text-grey-800 mb-4"> {post.title}</h2>
       <p className="text-gray-700 leading-relaxed mb-4">{post.content}</p>
     </div>
